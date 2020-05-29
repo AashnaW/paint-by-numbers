9b53b21b74c26f07e0f9ec5076bd7ed4c55c369b
Aashna Wadhwa <aashnaw2@illinois.edu>
2020-05-29 12:42:09 -0700

    original code

--- src/image.py
+++ src/image.py
@@ -2,21 +2,30 @@ from PIL import Image
 
 def get_image_color_count (im):
     w, h = im.size
-    pix = im.load()
     color_count = {}
 
     for i in range(w):
         for j in range(h):
             if pix[i,j] in color_count:
-                color_count[pix[i,j]] = color_count[pix[i,j]] + 1
+                color_count[im.getpixel((i, j))] = color_count[im.getpixel((i, j))] + 1
             else:
-                color_count[pix[i, j]] = 1
+                color_count[im.getpixel((i, j))] = 1
     return color_count
-    
 
-im = Image.open("../ironman.jpg")
+im = Image.open("../ironman.png")
 
-color_count = get_image_color_count(im)
+color_count = im.histogram()
 
-print (color_count)
+print(len(color_count))
+#for x in color_count:
+#    print(x)
  
+ 
+painting = Image.new(mode = "RGB", size = im.size)
+w, h = im.size
+for i in range(w):
+    for j in range(h):
+        painting.putpixel((i, j), im.getpixel((i, j)))
+
+print(painting.getcolors())
+painting.save("../ironman-painting.png")
